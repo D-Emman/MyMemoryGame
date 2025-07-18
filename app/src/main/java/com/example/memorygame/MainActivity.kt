304ee74afc8e9e3794c1704e010e8d8bf7e2f588
D-Emman <102307238+D-Emman@users.noreply.github.com>
2025-07-18 10:20:31 +0100

    cardlayout

--- app/src/main/java/com/example/memorygame/MainActivity.kt
+++ app/src/main/java/com/example/memorygame/MainActivity.kt
@@ -2,15 +2,25 @@ package com.example.memorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.memorygame.models.BoardSize
+import com.example.memorygame.models.MemoryCard
+import com.example.memorygame.models.MemoryGame
+import com.example.memorygame.utils.DEFAULT_ICONS
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val TAG = "MainActivity"
+    }
     private lateinit var rvBoard: RecyclerView
     private lateinit var tvNumMoves: TextView
     private lateinit var tvNumPairs: TextView
 
+    private var boardSize: BoardSize = BoardSize.EASY
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,9 +31,18 @@ class MainActivity : AppCompatActivity() {
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
 
-        rvBoard.adapter = MemoryBoardAdapter(this,8)
+        val memoryGame = MemoryGame(boardSize)
+
+
+        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CradClickListener{
+            override fun onCardClicked(position: Int) {
+                Log.i(TAG, "Card clicked $position")
+
+            }
+
+        } )
         rvBoard.setHasFixedSize(true)
-        rvBoard.layoutManager = GridLayoutManager( this, 2)
+        rvBoard.layoutManager = GridLayoutManager( this, boardSize.getWidth())
     }
 }
 
---
+++ app/src/main/java/com/example/memorygame/utils/Constants.kt
@@ -0,0 +1,22 @@
+package com.example.memorygame.utils
+
+import com.example.memorygame.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_basketball,
+    R.drawable.ic_car,
+    R.drawable.ic_cast_for_education,
+    R.drawable.ic_love,
+    R.drawable.ic_egg,
+    R.drawable.ic_female,
+    R.drawable.ic_key,
+    R.drawable.ic_male,
+    R.drawable.ic_money,
+    R.drawable.ic_star,
+    R.drawable.ic_sunny,
+    R.drawable.ic_icecream,
+    R.drawable.ic_church,
+    R.drawable.ic_restart,
+    R.drawable.ic_computer,
+    R.drawable.ic_casino
+)
