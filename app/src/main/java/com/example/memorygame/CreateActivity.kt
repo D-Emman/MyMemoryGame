3022001156055b2fefb769e8c6d3801f782b6e80
D-Emman <102307238+D-Emman@users.noreply.github.com>
2025-08-25 19:56:52 +0100

    Update CreateActivity.kt

--- app/src/main/java/com/example/memorygame/CreateActivity.kt
+++ app/src/main/java/com/example/memorygame/CreateActivity.kt
@@ -1,5 +1,4 @@
 package com.example.memorygame
-
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
@@ -15,7 +14,7 @@ import com.example.memorygame.adapters.SelectedImagesAdapter
 
 class CreateActivity : AppCompatActivity() {
 
-    // Set how many images are required for your board (adjust as needed)
+    // Set the number of images that are required for your board (adjust as needed)
     private var numImagesRequired = 6
 
     private lateinit var etGameName: EditText
